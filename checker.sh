8b147dc812366aea1da7812c7c9a4dfb389e2058
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2020-11-24 12:03:21 -0500

    checker.sh

--- checker.sh
+++ checker.sh
@@ -45,14 +45,3 @@ case $file in
         echo "its something else $file"
         ;;
 esac
-
-
-
-
-
-copyright="[*] *Copyright *[(][c][)](.*)*$date"
-while read line; do 
-    if [ ${#$line} -gt 88 ]; then echo "line longer than 88 characters"; exit 1; fi
-    if [[ $flag -eq 0 && $line =~ ${copyright} ]]; then flag=1; fi
-done < "$file"
-if [ $flag -eq 0 ]; then echo "Add copyright and update year"; exit 1; fi
