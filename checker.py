dd15b7665348d5c0e711684c5bd08e57909d7024
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-09 22:38:12 -0500

    Update checker.py

--- checker.py
+++ checker.py
@@ -37,13 +37,15 @@ def java_checker(file):
             if result:
                 years = result.groups()
                 if years[-1] != today.year:
-                    checks["license_message"] += "Update the license to the current year.\n"
+                    checks["license_message"] += "[ERROR] Update the license to the current year.\n"
                 if len(years) > 1 and years[0] >= years[-1]:
-                    checks["license_message"] += "Invalid years in license\n"
+                    checks["license_message"] += "[ERROR] Invalid years in license\n"
                 checks["license"] = False
-    print(checks["license_message"])
+
+    if checks['license_message']:
+        print(checks['license_message'])
     if checks["line_too_long"]:
-        print("The following lines are longer than 88 characters:")
+        print("[ERROR] The following lines are longer than 88 characters:")
         print(checks["line_too_long"])
     pass
 
@@ -74,23 +76,28 @@ def adoc_checker(file):
             if result:
                 years = result.groups()
                 if years[-1] != today.year:
-                    output += "Update the license to the current year.\n"
+                    output += "[ERROR] Update the license to the current year.\n"
                 if len(years) > 1 and years[0] >= years[-1]:
-                    output += "Invalid years in license\n"
+                    output += "[ERROR] Invalid years in license\n"
                 checks["license"] = False
         if checks["release_date"]:
             result = release_date_re.search(line)
             if result:
                 date = result.groups()
                 if not valid(date[-1]):
-                    output += f"Release date is invalid: {date[0]} at line {line_num + 1}\n"
-                    output += "The date should be in the form YYYY-MM-DD\n"
+                    output += f"[ERROR] Release date is invalid: {date[0]} at line {line_num + 1}\n"
+                    output += "[ERROR] The date should be in the form YYYY-MM-DD\n"
 
-    print(output)
+    if checks['license']:
+        output += '[ERROR] Add a license with the current year.\n'
+    if checks['release_date']:
+        output += '[ERROR] Add a release date.\n'  # do we want this?
+    if output != '':
+        print(output)
     if checks["lines"]:
-        print("The following lines are longer than 120 characters:")
+        print("[WARNING] The following lines are longer than 120 characters:")
         print(checks["lines"])
-        print("Consider wrapping text to improve readability.")
+        print("[WARNING] Consider wrapping text to improve readability.")
     pass
 
 
@@ -101,51 +108,31 @@ def html_checker(file):
     pass
 
 
-def filter_occurence(word):
-    return word[0] != 0
-
-
 def check_vocabulary(file, deny_list, warning_list):
     """
     """
     file.seek(0)
-    deny_occurence, warning_occurence = {}, {}
+    deny_occurrence, warning_occurrence = {}, {}
     deny = re.compile('|'.join(map(re.escape, deny_list)))
     warn = re.compile('|'.join(map(re.escape, warning_list)))
 
-    # for word in deny_list:
-    #     deny_occurence[word] = [0, []]
-    # for word in warning_list:
-    #     warning_occurence[word] = [0, []]
     for line_num, line in enumerate(file):
-        a = list(filter(lambda word: deny.search(word), line.split()))
-        b = list(filter(lambda word: warn.search(word), line.split()))
+        line = re.sub('[^0-9a-zA-Z]+', ' ', line).split()
+        # print(line)
+        a = list(filter(lambda word: deny.fullmatch(word), line))
+        b = list(filter(lambda word: warn.fullmatch(word), line))
         if a:
-            deny_occurence[line_num] = a
+            deny_occurrence[line_num] = a
         if b:
-            warning_occurence[line_num] = b
-    print(deny_occurence, warning_occurence)
-    # file.seek(0)
-    # deny_occurence, warning_occurence = {}, {}
-    # for word in deny_list:
-    #     deny_occurence[word] = [0, []]
-    # for word in warning_list:
-    #     warning_occurence[word] = [0, []]
-
-    # for line_num, line in enumerate(file):
-    #     words = line.split(' ')
-    #     if line_num == 25:
-    #         print(line)
-    #         print(words)
-    #     for word in words:
-    #         if word in deny_list:
-    #             deny_occurence[word][0] += 1
-    #             deny_occurence[word][1].append(line_num)
-    #         if word in warning_list:
-    #             warning_occurence[word][0] += 1
-    #             warning_occurence[word][1].append(line_num)
-    # print(deny_occurence)
-    # return filter(filter_occurence, deny_occurence), filter(filter_occurence, warning_occurence)
+            warning_occurrence[line_num] = b
+    if deny_occurrence:
+        print('\n[ERROR] The following words must be changed.')
+        for line in deny_occurrence.keys():
+            print(f'[ERROR] Line {line}: {deny_occurrence[line]}')
+    if warning_occurrence:
+        print('\n[WARNING] The following words should ideally be changed.')
+        for line in warning_occurrence.keys():
+            print(f'[WARNING] Line {line}: {warning_occurrence[line]}')
 
 
 if __name__ == "__main__":
@@ -157,16 +144,18 @@ if __name__ == "__main__":
     parser.add_argument('infile', nargs='*',
                         type=argparse.FileType('r'), default=sys.stdin)
     args = parser.parse_args()
-    deny_list = json.loads(args.deny[0].read())
-    warning_list = json.loads(args.warn[0].read())
-    # if args.deny is not None:
-    #     deny_list = json.loads(args.deny[0].read())
-    #     deny = re.compile('|'.join(map(re.escape, deny_list)))
-    # if args.warn is not None:
-    #     warning_list = json.loads(args.warn[0].read())
-    #     warn = re.compile('|'.join(map(re.escape, warning_list)))
-
-    # timeline = filter(lambda word: not deny.search(word), status.split())
+    # check if var exist and NON empty
+    if args.deny is not None:
+        try:
+            deny_list = json.loads(args.deny[0].read())
+        except:
+            deny_list = []
+    if args.warn is not None:
+        try:
+            warning_list = json.loads(args.warn[0].read())
+        except:
+            Warning_list = []
+    # print(warning_list)
 
     for file in args.infile:
         file_extension = file.name.split('/')[-1].split('.')[-1]
@@ -175,11 +164,9 @@ if __name__ == "__main__":
             adoc_checker(file)
         elif file_extension == 'java':
             java_checker(file)
-
         elif file_extension == 'html':
             html_checker(file)
-
         else:
             print('its something else')
-        # check vocabulary
+
         check_vocabulary(file, deny_list, warning_list)
